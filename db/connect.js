cf715b4d4c612a78bd8f51fcb9a455fa41d762c6
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-10-05 00:06:34 -0300

    added debug line to mongodb connection

--- db/connect.js
+++ db/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
+    debug: true,
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
