9476c22094dd05138c67eb7d1379cbbf9812a4f4
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-10-24 03:19:25 -0300

    created job schema

---
+++ models/Job.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const JobSchema = mongoose.Schema(
+  {
+    company: {
+        type: String,
+        require: [true, 'Please provide company name'],
+        maxlength: 50
+    },
+    position: {
+        type: String,
+        required: [true, 'Please provide position']
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user']
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Job', JobSchema)
